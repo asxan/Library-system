c328308769bdbec4a83f3cdf7b512b807b4747ec
Alexander Lyashenko <movinguser@gmail.com>
2021-06-06 20:04:21 +0300

    some changes

--- library-api/index.js
+++ library-api/index.js
@@ -8,10 +8,13 @@ import cors from 'cors'
 
 const PORT = process.env.PORT || 3000
 const app = express();
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
 
 app.use(cors());
-
-
 app.use(booksRouter);
 app.use(usersRouter);
 app.use(orderRouter);
@@ -19,20 +22,14 @@ app.use(statsRouter);
 
 async function start() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/library',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      });
-
-    console.log('\n');
-    console.log('\x1b[32mDB has been connected...\x1b[0m');
-    
+    console.log('\n\x1b[32m[app] starting...');
+    await mongoose.connect('mongodb://localhost:27017/library',dbOptions);
+    console.log('\x1b[32m[app] app has been to db connected\x1b[0m');
     app.listen(PORT, () => {
-      console.log("\x1b[32mAPI has been started...\x1b[0m");
+      console.log("\x1b[32m[app] listening has been started\x1b[0m");
     });
   } catch (e) {
-    console.log(e);
+    console.log("\x1b[31m[app] " + e + "\x1b[0m");
   }
 }
 start()
