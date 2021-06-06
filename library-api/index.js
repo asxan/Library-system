fac4f43f2309dbd89fcc680b2e179636aa66dfa1
Alexander Lyashenko <movinguser@gmail.com>
2021-06-07 01:33:16 +0300

    Refactored index.js

--- library-api/index.js
+++ library-api/index.js
@@ -1,18 +1,20 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import booksRouter from './routers/booksRouters.js'
-import usersRouter from './routers/usersRouters.js'
-import orderRouter from './routers/orderRouters.js'
-import statsRouter from './routers/statsRouter.js'
-import cors from 'cors'
+import express from 'express';
+import mongoose from 'mongoose';
+import booksRouter from './routers/booksRouters.js';
+import usersRouter from './routers/usersRouters.js';
+import orderRouter from './routers/orderRouters.js';
+import statsRouter from './routers/statsRouter.js';
+import cors from 'cors';
 
-const PORT = process.env.PORT || 3000
-const app = express();
-const dbOptions = {
+const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/library';
+const DB_OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 }
+
+const app = express();
 
 app.use(cors());
 app.use(booksRouter);
@@ -20,16 +22,14 @@ app.use(usersRouter);
 app.use(orderRouter);
 app.use(statsRouter);
 
-async function start() {
-  try {
-    console.log('\n\x1b[32m[app] starting...');
-    await mongoose.connect('mongodb://localhost:27017/library',dbOptions);
-    console.log('\x1b[32m[app] app has been to db connected\x1b[0m');
-    app.listen(PORT, () => {
-      console.log("\x1b[32m[app] listening has been started\x1b[0m");
-    });
-  } catch (e) {
-    console.log("\x1b[31m[app] " + e + "\x1b[0m");
-  }
+try {
+  console.log('\n\x1b[32m[app] starting...');
+
+  await mongoose.connect(DB_URL, DB_OPTIONS);
+
+  console.log('\x1b[32m[app] app has been to db connected\x1b[0m');
+
+  app.listen(PORT, () => console.log("\x1b[32m[app] listening has been started\x1b[0m"));
+} catch (e) {
+  console.log("\x1b[31m[app] " + e + "\x1b[0m");
 }
-start()
